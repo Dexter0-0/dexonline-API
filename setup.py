171a2a4ee4dc9d368f1a34ab128448c60bc51beb
Dexter0-0 <56682055+Dexter0-0@users.noreply.github.com>
2020-05-05 10:11:12 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup setup(
+  name = 'dexonline',         
+  packages = ['dexonline'],   
+  version = '0.1',
+  license='MIT',
+  description = 'API neoficial pentru https://dexonline.ro',
+  author = 'Nandra Rares',
+  author_email = 'nandrarares16@gmail.com',
+  url = '',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
