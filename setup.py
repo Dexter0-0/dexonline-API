194ca2b80e79c4651b935b009f819fe9942fc552
Dexter0-0 <56682055+Dexter0-0@users.noreply.github.com>
2020-05-05 10:19:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,9 +15,7 @@ from distutils.core import setup setup(
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
   ],
 )
